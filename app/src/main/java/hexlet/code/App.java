faea037ad14c652c25967c01ef9b72bd73ddb8d8
razmikl <luluckyanr@yandex.ru>
2024-11-24 17:20:30 +0300

    update build.gradle and update program with help-command

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.*;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "ProjectV 1.0", description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable{
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean helpRequested;
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionRequested;
+
+
     public static void main(String[] args) {
+        CommandLine cmd = new CommandLine(new App());
+        cmd.execute(args);
+    }
+
+    @Override
+    public void run() {
         System.out.println("Hello, World!");
     }
 }
