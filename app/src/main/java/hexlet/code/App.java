73c85eb0e735a168cc164374b7e51630c3f7136b
razmikl <luluckyanr@yandex.ru>
2024-12-21 11:54:23 +0300

    update helpMessage

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,12 +3,26 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 
+import java.io.File;
+import java.text.Format;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "ProjectV 1.0", description = "Compares two configuration files and shows a difference.")
 
 public class App implements Runnable{
+
+    @Parameters(description = "path to first file")
+    private File filepath1;
+
+    @Parameters(description = "path to second file")
+    private File filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    Format format;
+
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean helpRequested;
+
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionRequested;
 
@@ -20,6 +34,9 @@ public class App implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Hello, World!");
+        if (helpRequested) {
+            System.out.println("Help message");
+            return;
+        }
     }
 }
