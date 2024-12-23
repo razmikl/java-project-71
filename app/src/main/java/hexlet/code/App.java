bb648352d72fd47f02e65113685125288e0cab15
razmikl <luluckyanr@yandex.ru>
2024-12-23 18:16:11 +0300

    update App.java, parsing code

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,13 @@ import picocli.CommandLine;
 import picocli.CommandLine.*;
 
 import java.io.File;
+import java.sql.SQLOutput;
 import java.text.Format;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "ProjectV 1.0", description = "Compares two configuration files and shows a difference.")
@@ -27,9 +33,27 @@ public class App implements Runnable{
     boolean versionRequested;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CommandLine commandLine = new CommandLine(new App());
         commandLine.execute(args);
+
+        var parseResult = new HashMap<String, String>();
+
+        String readFile1Path = "/home/razmik/java-project-71/app/file1.json";
+        String readFil2Path = "/home/razmik/java-project-71/app/file2.json";
+
+        Path pathFile1 = Paths.get(readFile1Path).toAbsolutePath().normalize();
+        Path pathFile2 = Paths.get(readFil2Path).toAbsolutePath().normalize();
+
+        String contentFile1 = Files.readString(pathFile1);
+        parseResult.put(readFile1Path, contentFile1);
+        String contentFile2 = Files.readString(pathFile2);
+        parseResult.put(readFil2Path, contentFile2);
+
+        System.out.println(parseResult.get(readFile1Path));
+        System.out.println(parseResult.get(readFil2Path));
+
+
     }
 
     @Override
